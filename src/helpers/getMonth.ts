843999e81b38cae8035f112b42e482d39c74e94e
mxmln-wysk <maximilian.wysk@gmx.net>
2022-01-02 13:53:33 +0100

    adjust format date

--- src/helpers/getMonth.ts
+++ src/helpers/getMonth.ts
@@ -2,7 +2,6 @@ import electron from 'electron'
 const ipc = electron.ipcRenderer;
 
 const getMonth = async (date:string) => {
-    console.log('getMonth')
     return ipc.invoke('getMonth',date)
     .then((response) =>{        
         return JSON.parse(response);
